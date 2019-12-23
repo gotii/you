00652c9da1d751f3f2a4ce66d2f26dbe333f388b
Gourav Soni <52281206+gouravbro@users.noreply.github.com>
2019-12-23 21:52:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// window.onload = function () {
+//     if (window.jQuery) {
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+
+
+// $(document).ready(function () {
+// $("input")
+//     .keyup(function () {
+//         var value = $(this).val();
+//         $("p").text(value);
+//     })
+//     .keyup();
+
+
+// });
+
+// $("btn").click({function displayVals() {
+//         var inpt = $("#inpt").val();
+//         var para = $("#para").val();
+//         $("p").html(inpt);
+//     }
+
+//     $("p").change(displayVals);
+//     displayVals();
+// )};
